c6200c67cb49c2eb07f6cec6bb682caf5488222a
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2025-06-06 19:06:09 -0500

    refactor: datail page

--- resources/js/components/form/FormInquire.vue
+++ resources/js/components/form/FormInquire.vue
@@ -310,7 +310,18 @@
                     },
                     {
                         nombre: 'Montaña 7 colores', value: '4'
-                    }
+                    },
+                    {
+                        nombre: 'Lago Titicaca', value: '5'
+                    },
+                    {
+                        nombre: 'Lago Titicaca', value: '6'
+                    },
+                    {
+                        nombre: 'Lago Titicaca', value: '7'
+                    },
+                    {
+                        nombre: 'Lago Titicaca', value: '8+'
                     }
                 ],
                 durations_form:[
